1a834d6d93adb8cb7d37edecb4c5efcd8f09999e
Jacqueline Faustino Vitor <jacqueline.vitor@nextel.com.br>
2018-03-02 11:29:05 -0300

    POC

--- spec.js
+++ spec.js
@@ -1,8 +1,12 @@
 // spec.js
+
+//import page from 'esquadro';
+const page = require('esquadro/page');
 describe('Planos online website', function() {
   var detailBtn = element(by.css(".btnSaiba"));
   var boxFranchise = element(by.css(".box-franchise"));
-  var purchaseBtn = element(by.css('[type="submit"]'));
+  //var purchaseBtn = element(by.css('[type="submit"]'));
+  var purchaseBtn = element(by.xpath('//*[@id="frmDetail"]/button'));
   var checkoutPage = element(by.css(".contR"));
 
   beforeEach(function() {
@@ -19,8 +23,10 @@ it('get to see the details of a plan', function(){
 });
 
 it('should have a purchase button', function() {
-
+page.waitForElementToBeClickable(purchaseBtn);
+console.log(purchaseBtn.getText());
   expect(boxFranchise.isPresent());
+
  browser.executeScript("arguments[0].click();", purchaseBtn);
 
 });
