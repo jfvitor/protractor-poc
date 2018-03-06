7f9f633c067d6e73db4326ef5d7b8e507813e2d6
Jacqueline Faustino Vitor <jacqueline.vitor@nextel.com.br>
2018-03-06 18:52:08 -0300

    Edit config

--- conf.js
+++ conf.js
@@ -1,9 +1,13 @@
 exports.config = {
   framework: 'jasmine',
   directConnect: true,
-  specs: ['spec.js'],
-  onPrepare: function () {
-      browser.driver.manage().window().maximize()
-     // browser.ignoreSynchronization = true;
+  specs: [
+    'step_definitions/home_spec.js',
+    'step_definitions/details_spec.js'
+  ],
+  onPrepare: function() {
+    browser.driver.manage().window().setSize(1680, 1050);
+    //browser.driver.manage().window().maximize()
+    // browser.ignoreSynchronization = true;
   }
 };
