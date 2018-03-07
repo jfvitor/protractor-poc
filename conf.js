d464740f12f6621762b6eff1e673807768c98fa9
jvitor <jacquevitor@gmail.com>
2018-03-07 16:05:26 -0300

    Edit conf.js

--- conf.js
+++ conf.js
@@ -2,12 +2,13 @@ exports.config = {
   framework: 'jasmine',
   directConnect: true,
   specs: [
-    'step_definitions/home_spec.js',
-    'step_definitions/details_spec.js'
+    'feature/step_definitions/home_spec.js',
+    'feature/step_definitions/details_spec.js'
   ],
   onPrepare: function() {
     browser.driver.manage().window().setSize(1680, 1050);
     //browser.driver.manage().window().maximize()
     // browser.ignoreSynchronization = true;
+     jasmine.getEnv().addReporter(reporter);
   }
 };
