0477377751303fdad2671183a7f4ad27ef68cbf2
jvitor <jacquevitor@gmail.com>
2018-03-07 16:22:37 -0300

    Delete files

--- pages/details_page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var Detail = function() {
-
-    this.boxFranchise = element(by.css(".box-franchise"));
-    this.purchaseBtn = element(by.css('[type="submit"]'));
-    this.checkoutPage = element(by.css(".contR"));
-};
-    module.exports = Detail;
