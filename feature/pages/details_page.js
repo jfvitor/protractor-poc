b2a8bdb7c3af9d8794ffaca602264abb79e1b91f
Jacqueline Faustino Vitor <jacqueline.vitor@nextel.com.br>
2018-03-08 14:13:32 -0300

    Add new scenarios

--- feature/pages/details_page.js
+++ feature/pages/details_page.js
@@ -5,5 +5,19 @@ var Detail = function() {
     this.boxFranchise = element(by.css(".box-franchise"));
     this.purchaseBtn = element(by.css('[type="submit"]'));
     this.checkoutPage = element(by.css(".contR"));
+
+    //post paid plans
+    this.detailsP = element(by.xpath('/html/body/section[1]/div/div/div[2]/div[1]/div/h2[1]'));
+    this.detailsM = element(by.xpath('/html/body/section[1]/div/div/div[2]/div[1]/div/h2[2]'));
+    this.detailsG = element(by.xpath('/html/body/section[1]/div/div/div[2]/div[1]/div/h2[3]'));
+    this.detailsGG = element(by.xpath('/html/body/section[1]/div/div/div[2]/div[1]/div/h2[4]'));
+
+    //control plans
+    this.detailscontrol1 = element(by.xpath('/html/body/section[1]/div/div/div[2]/div[1]/div/h2[1]'));
+    this.detailscontrol2 = element(by.xpath('/html/body/section[1]/div/div/div[2]/div[1]/div/h2[2]'));
+    this.detailscontrol3 = element(by.xpath('/html/body/section[1]/div/div/div[2]/div[1]/div/h2[3]'));
+
+    this.changePlans = element(by.css('.link-p'));
+
 };
-    module.exports = Detail;
+module.exports = Detail;
--- feature/step_definitions/details_spec.js
+++ feature/step_definitions/details_spec.js
@@ -3,26 +3,50 @@ const HomePage = require('../pages/home_page');
 const Detail = require('../pages/details_page');
 
 describe('Planos online website', function() {
-  var homepage = new HomePage();
-  var detail = new Detail();
+    var homepage = new HomePage();
+    var detail = new Detail();
 
-  beforeAll(function() {
-    browser.get('http://planosonline-hmg.nexteldigital.com.br');
-  });
+    beforeAll(function() {
+        browser.get('http://planosonline-hmg.nexteldigital.com.br');
+    });
 
-  it('should go to the details page', function() { // declara que o escopo trabalha de modo assincrono
-    homepage.detailBtn.click();
-  });
+    it('should go to the details page', function() { // declara que o escopo trabalha de modo assincrono
+        homepage.detailBtn.click();
+    });
 
-  it('should have a purchase button', async () => { // declara que o escopo trabalha de modo assincrono
-    handles = await browser.getAllWindowHandles(); // aguarda a execução deste bloco
-    browser.switchTo().window(handles[1]); // pega o segundo item (indice 1) que é a nova aba
-    page.waitForElementToBeClickable(detail.purchaseBtn); // usa uma Expected Condition de botão renderizado
-    detail.purchaseBtn.click();
-  });
+    it('should change to the Details page', async() => { // declara que o escopo trabalha de modo assincrono
+        handles = await browser.getAllWindowHandles(); // aguarda a execução deste bloco
+        browser.switchTo().window(handles[1]); // pega o segundo item (indice 1) que é a nova aba
+        page.waitForElementToBeClickable(detail.purchaseBtn); // usa uma Expected Condition de botão renderizado
+        expect(browser.getCurrentUrl()).toBe("http://planosonline-hmg.nexteldigital.com.br/plano/pos/p");
 
-  it('should go to the checkout page', function() {
-    expect(browser.getCurrentUrl()).toBe("http://planosonline-hmg.nexteldigital.com.br/plano/checkout");
-  });
+    });
+    it('should have the option to navigate among the Post Paid plans', async() => { // declara que o escopo trabalha de modo assincrono
+        handles = await browser.getAllWindowHandles(); // aguarda a execução deste bloco
+        browser.switchTo().window(handles[1]); // pega o segundo item (indice 1) que é a nova aba
+        page.waitForElementToBeClickable(detail.purchaseBtn); // usa uma Expected Condition de botão renderizado
+        detail.detailsM.click();
+        detail.detailsP.click();
+        detail.detailsG.click();
+        detail.detailsGG.click();
+    });
+
+    it('should change to Control Plans details page', function() {
+        detail.changePlans.click();
+    });
+
+    it('should have the option to navigate among the Control plans', function() {
+        detail.detailscontrol2.click();
+        detail.detailscontrol3.click();
+        detail.detailscontrol1.click();
+    });
+
+    it('should have a purchase button', function() { // declara que o escopo trabalha de modo assincrono
+        detail.purchaseBtn.click();
+    });
+
+    it('should go to the checkout page', function() {
+        expect(browser.getCurrentUrl()).toBe("http://planosonline-hmg.nexteldigital.com.br/plano/checkout");
+    });
 
 });
