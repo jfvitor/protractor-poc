48f8aeab59fcee5b2df8e9f1f660ae13cc575fe4
jvitor <jacquevitor@gmail.com>
2018-04-19 15:42:06 -0300

    Edit step definitions

--- feature/step_definitions/details_spec.js
+++ feature/step_definitions/details_spec.js
@@ -1,11 +1,12 @@
-//const page = require('esquadro/page');
 const HomePage = require('../pages/home_page');
 const Detail = require('../pages/details_page');
+const CheckOutPage = require('../pages/checkout_page');
 
-describe('Planos online website', () => {
+describe('Checking the details of a plan', () => {
+    var checkoutpage = new CheckOutPage();
     var homepage = new HomePage();
     var detail = new Detail();
-
+    
     beforeAll(async() => {
         browser.get('http://planosonline-hmg.nexteldigital.com.br');
     });
@@ -17,18 +18,16 @@ describe('Planos online website', () => {
         await expect(browser.getCurrentUrl()).toBe("http://planosonline-hmg.nexteldigital.com.br/plano/pos/p");
     });
 
-    it('should have the option to navigate among the Post Paid plans', async() => { // declara que o escopo trabalha de modo assincrono
+    it('should have the option to navigate among the bill pay plans', async() => {
         await expect(detail.detailsList.isPresent()).toEqual(true)
         await detail.detailsM.click();
         await expect(browser.getTitle()).toEqual('Planos Online - POS M');
-        //  page.waitForElementToBeClickable(detail.purchaseBtn); 
         await detail.detailsP.click();
         await expect(browser.getTitle()).toEqual('Planos Online - POS P');
         await detail.detailsG.click();
         await expect(browser.getTitle()).toEqual('Planos Online - POS G');
         await detail.detailsGG.click();
         await expect(browser.getTitle()).toEqual('Planos Online - POS GG');
-
     });
 
     it('should change to Control Plans details page', async() => {
@@ -43,15 +42,14 @@ describe('Planos online website', () => {
         await expect(browser.getTitle()).toEqual('Planos Online - CONTROLE 3GB');
     });
 
-    it('should have a purchase button', async() => { // declara que o escopo trabalha de modo assincrono
+    it('should have a purchase button', async() => {
         await expect(detail.purchaseBtn.isPresent()).toEqual(true)
         await detail.purchaseBtn.click();
-
     });
 
     it('should go to the checkout page', async() => {
         await expect(browser.getCurrentUrl()).toBe("http://planosonline-hmg.nexteldigital.com.br/plano/checkout");
-        await expect(detail.step0.isPresent()).toEqual(true)
+        await expect(checkoutpage.step0.isPresent()).toEqual(true)
         await detail.step0.click();
     });
 
